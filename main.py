9311f3b5dc9c3346e0ca5b8797ada94590114753
abigail <iyalolaoluwatosin@gmail.com>
2026-03-08 20:54:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import requests
-from config import HF_API_KEY
+from PIL import Image
+from io import BytesIO
+from config import API_KEY
 
-def classify_text(text):
-    url = "https://router.huggingface.co/hf-inference/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
-    headers = {"Authorization": f"Bearer {HF_API_KEY}"}
-    payload = {"inputs": text}
-    response = requests.post(url, headers=headers, json=payload)
+API_URL="https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-3-medium-diffusers"
+
+def generate_image_from_text(promt: str)->Image.Image:
+    header={"Authorization": f"Bearer {API_KEY}"}
+    payload={"inputs":promt}
+
     try:
-        return response.json()
-    except requests.exceptions.JSONDecodeError:
-       print(f"Requests failed with status code {response.status_code}")
-       print(f"Response text: {response.text}")
-       return{}
+        response=requests.post(API_URL, headers=header, json=payload,timeout=30)
+        response.raise_for_status()
+
+        if 'image' in response.headers.get('Content-Type', ''):
+            image=Image.open(BytesIO(response.content))
+            return image
+        else:
+            raise Exception("the response is not an image, might be a error message")
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Request error: {e}")
     
-if __name__ == "__main__":
-    sample_text = "I love using Hugging Face APIs!"
-    result = classify_text(sample_text)
-    print(result)
+def main():
+    print("==WELCOME TO IMAGE GENERATOR==")
+    print("Type 'exit' to quit the program\n")
+
+    while True:
+        prompt=input("Enter a description for the image you want to generate:\n ").strip()
+        if prompt.lower()=="exit":
+            print("Thank you for using oyr program")
+            break
+        print("generating image.......")                                            
+    try:
+            image=generate_image_from_text(prompt)
+            image.show()
+
+            save_option=input("Do you want to save the image? (yes/no): ").strip().lower()
+            if save_option=='yes':
+                file_name=input("enter image name: ").strip()
+                file_name="".join(c for c in file_name if c.isalnum() or c in ('_',"-")).strip()
+                image.save(f"{file_name}.png")
+                print(f"image saved as {file_name}.png")
+            else:
+                print("image not saved")
+    except Exception as e:
+            print(f"An error occured: {e}")
+            
+if __name__=="__main__":
+    main()
+
+
